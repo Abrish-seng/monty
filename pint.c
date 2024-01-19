d7866fcf228670b771c9f4f1b996fcf657ca5d8a
abrish-seng <abrish814@gmail.com>
2024-01-18 22:33:48 -0800

    pint added

---
+++ pint.c
@@ -0,0 +1,21 @@
+
+#include "monty.h"
+/**
+ * print_stack - Adds a node to the stack.
+ * @stack: Pointer to a pointer pointing to top node of the stack.
+ * @line_number: line number of  the opcode.
+ */
+void print_stack(stack_t **stack, unsigned int line_number)
+{
+	stack_t *tmp;
+
+	(void) line_number;
+	if (stack == NULL)
+		exit(EXIT_FAILURE);
+	tmp = *stack;
+	while (tmp != NULL)
+	{
+		printf("%d\n", tmp->n);
+		tmp = tmp->next;
+	}
+}
