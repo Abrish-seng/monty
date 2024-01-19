7ac63f6043adf735058f844f0f11f9646c72ea53
abrish-seng <abrish814@gmail.com>
2024-01-18 22:38:58 -0800

    swap added

---
+++ swap.c
@@ -0,0 +1,21 @@
+#include "monty.h"
+/**
+ * swap_nodes - Swaps the top two elements of the stack.
+ * @stack: Pointer to a pointer pointing to top node of the stack.
+ * @line_number: Interger representing the line number of of the opcode.
+ */
+void swap_nodes(stack_t **stack, unsigned int line_number)
+{
+	stack_t *tmp;
+
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+		printf("L%d: can't pop an empty stack\n", line_number);
+	tmp = (*stack)->next;
+	(*stack)->next = tmp->next;
+	if (tmp->next != NULL)
+		tmp->next->prev = *stack;
+	tmp->next = *stack;
+	(*stack)->prev = tmp;
+	tmp->prev = NULL;
+	*stack = tmp;
+}
